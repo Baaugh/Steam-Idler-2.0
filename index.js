01159f4e75a47e23a74e4926532080cf8ab212c7
Baaugh <111416915+Baaugh@users.noreply.github.com>
2025-08-26 01:52:29 -0400

    Updated Games

--- index.js
+++ index.js
@@ -6,7 +6,7 @@ var username = process.env.username;
 var password = process.env.password;
 var shared_secret = process.env.shared;
 
-var games = [730, 440, 570];  // Enter here AppIDs of the needed games
+var games = [311210, 2357570, 1928420, 1517290, 2943650, 10090, 1468260, 1727650, 1190340];  // Enter here AppIDs of the needed games
 var status = 1;  // 1 - online, 7 - invisible
 
 
